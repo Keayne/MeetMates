be4e374a36428b41d35ad98f542e3243ccd4bf73
as722905 <as722905@fh-muenster.de>
2022-05-17 19:55:37 +0200

    create post request for new activity

--- client/src/components/find-activity/create-activity/new-activity.ts
+++ client/src/components/find-activity/create-activity/new-activity.ts
@@ -2,9 +2,9 @@
 
 import { LitElement, html } from 'lit';
 import { customElement, property, query } from 'lit/decorators.js';
+import { httpClient } from '../../../http-client';
 import { PageMixin } from '../../page.mixin';
 import { Actitity } from '../find-activity';
-import { httpClient } from '../../../http-client';
 
 import componentStyle from './new-activity.css';
 
@@ -14,9 +14,25 @@ class CardComponent extends PageMixin(LitElement) {
 
   @property({ reflect: true }) activity = {} as Actitity;
   @query('#myForm') private myForm!: HTMLDivElement;
+  @query('#title') private titlee!: HTMLInputElement;
+  @query('#description') private description!: HTMLInputElement;
+  @query('#motivationTitle') private motivationTitle!: HTMLInputElement;
 
-  async postActivity() {
-    await httpClient.get('activity' + location.search);
+  async submit(event: Event) {
+    event.preventDefault();
+    const partialActivity: Partial<Actitity> = {
+      title: this.titlee.value,
+      description: this.description.value,
+      motivationtitle: this.motivationTitle.value
+    };
+    try {
+      const response = await httpClient.post('/activity', partialActivity);
+      this.titlee.value = '';
+      this.description.value = '';
+      this.motivationTitle.value = '';
+    } catch (e) {
+      this.showNotification((e as Error).message, 'error');
+    }
   }
 
   render() {
@@ -24,17 +40,23 @@ class CardComponent extends PageMixin(LitElement) {
       <!-- Pop up -->
       <button class="open-button" @click="${this.openForm}">Create Activity</button>
       <div class="form-popup" id="myForm">
-        <form action="${this.postActivity}" class="form-container">
+        <form @submit="${this.submit}" class="form-container">
           <h1>New Activity</h1>
 
           <label for="title"><b>Title</b></label>
-          <input type="text" placeholder="Enter Title" name="title" required />
+          <input type="text" placeholder="Enter Title" name="title" id="title" required />
 
           <label for="description"><b>Description</b></label>
-          <input type="text" placeholder="Enter Description" name="description" required />
+          <input type="text" placeholder="Enter Description" name="description" id="description" required />
 
           <label for="description"><b>Motivation Title</b></label>
-          <input type="text" placeholder="Enter Motivation Title" name="motivationTitle" required />
+          <input
+            type="text"
+            placeholder="Enter Motivation Title"
+            name="motivationTitle"
+            id="motivationTitle"
+            required
+          />
 
           <button type="submit" class="btn">Create Actitity</button>
           <button type="button" class="btn cancel" @click="${this.closeForm}">Close</button>
@@ -44,7 +66,6 @@ class CardComponent extends PageMixin(LitElement) {
     `;
   }
   openForm() {
-    console.log('openFormFunction ');
     this.myForm.style.display = 'block'; //TODO can't find object
   }
 
