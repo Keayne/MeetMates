015fd9443a1a83e86050579a9e2e73458fa5bf5d
as722905 <as722905@fh-muenster.de>
2022-06-16 10:50:07 +0200

    fixed security bugs, added tests

---
+++ api-server/e2e/src/activity.e2e-spec.ts
@@ -0,0 +1,167 @@
+/* Autor: Arne Schaper */
+
+import { expect } from 'chai';
+import { Response } from 'node-fetch';
+import { Activity } from '../../src/models/activity.js';
+import { UserSession } from './user-session.js';
+
+interface Meet {
+  id: string;
+  name: string;
+  opened: boolean;
+  mates: Mate[];
+}
+interface Mate {
+  id: string;
+  name: string;
+  src: string;
+  age: string;
+}
+
+describe('activity', () => {
+  let userSession: UserSession;
+
+  beforeEach(async () => {
+    userSession = new UserSession();
+  });
+
+  afterEach(async () => {
+    await userSession.deleteUser();
+  });
+
+  describe('Fail to login', () => {
+    it('should fail given that the user is not logged in.', async () => {
+      const response = await userSession.get(`/activity/0e8309e1-76eb-4eb9-9558-704b9358309d`);
+      expect(response.status).to.equal(401);
+    });
+
+    it('should fail to post an activity as the user is not logged in.', async () => {
+      const activity: Partial<Activity> = {
+        title: 'title',
+        description: 'desc',
+        tooltipcreatedby: '1234',
+        motivationtitle: 'motTitle',
+        meetid: ``,
+        image: '',
+        category: 'Sport'
+      };
+      const post = await userSession.post(`/activity/b3b9f92b-3845-479b-a896-540c049ab1b3`, activity);
+      expect(post.status).to.equal(401);
+    });
+
+    it('should fail to delete an activity as the user is not logged in.', async () => {
+      const deleteReq = await userSession.delete(`/activity/db1a0c51-8f60-46c2-af93-b84204166610`);
+      expect(deleteReq.status).to.equal(401);
+    });
+  });
+
+  describe('Get activities for meet', () => {
+    let meetIds: string[] = [];
+    let meetResponse: Response | null;
+    beforeEach(async () => {
+      userSession = new UserSession();
+      await userSession.registerUser();
+      await userSession.get('/meets/');
+      meetResponse = await userSession.get('/meets/');
+      const meets = (await meetResponse.json()) as Array<Meet>;
+      if (meets.length !== 0) {
+        meets.forEach(meet => {
+          meetIds.push(meet.id);
+        });
+      }
+    });
+
+    afterEach(async () => {
+      await userSession.deleteUser();
+      meetIds = [];
+      meetResponse = null;
+    });
+
+    it('should be able to access find activity page for correct meetId', async () => {
+      const redirect = await userSession.get(`/activity/${meetIds[0]}`);
+      if (meetResponse != null) {
+        expect(redirect.status).to.equal(200);
+      } else {
+        expect(meetResponse).to.not.be.equal(null);
+      }
+    });
+
+    it('should fail given that the MeetId is not present.', async () => {
+      const response = await userSession.get(`/activity/0e8309e1-76eb-4eb9-9558-704b9358309d`);
+      expect(response.status).to.equal(404);
+    });
+  });
+
+  describe('Create and delete activities for a meet', () => {
+    let meetIds: string[] = [];
+    let meetResponse: Response | null;
+    beforeEach(async () => {
+      userSession = new UserSession();
+      await userSession.registerUser();
+      await userSession.get('/meets/');
+      meetResponse = await userSession.get('/meets/');
+      const meets = (await meetResponse.json()) as Array<Meet>;
+      if (meets.length !== 0) {
+        meets.forEach(meet => {
+          meetIds.push(meet.id);
+        });
+      }
+    });
+
+    afterEach(async () => {
+      await userSession.deleteUser();
+      meetIds = [];
+      meetResponse = null;
+    });
+
+    it('should be able to create a new activity', async () => {
+      if (meetResponse === null) expect(meetResponse).to.not.be.equal(null);
+      const activity: Partial<Activity> = {
+        title: 'title',
+        description: 'desc',
+        tooltipcreatedby: '1234',
+        motivationtitle: 'motTitle',
+        meetid: `${meetIds[0]}`,
+        image: '',
+        category: 'Sport'
+      };
+      const post = await userSession.post(`/activity/${meetIds[0]}`, activity); //TODO make sure redirect is valid and doenst return error
+      expect(post.status).to.equal(201);
+    });
+
+    it('should fail to post a new activity for a meet that doesnt exist.', async () => {
+      if (meetResponse === null) expect(meetResponse).to.not.be.equal(null);
+      const activity: Partial<Activity> = {
+        title: 'title',
+        description: 'desc',
+        tooltipcreatedby: '1234',
+        motivationtitle: 'motTitle',
+        meetid: `${meetIds[0]}`,
+        image: '',
+        category: 'Sport'
+      };
+      const post = await userSession.post(`/activity/b3b9f92b-3845-479b-a896-540c049ab1b3`, activity); //TODO make sure redirect is valid and doenst return error
+      expect(post.status).to.equal(404);
+    });
+
+    /*
+    it('should create an activity for a meet, then successfully get all activities, and finally delete it.', async () => {
+      if (meetResponse === null) expect(meetResponse).to.not.be.equal(null);
+      const activity: Partial<Activity> = {
+        title: 'title',
+        description: 'desc',
+        tooltipcreatedby: '1234',
+        motivationtitle: 'motTitle',
+        meetid: `${meetIds[0]}`,
+        image: '',
+        category: 'Sport'
+      };
+      const post = await userSession.post(`/activity/b3b9f92b-3845-479b-a896-540c049ab1b3`, activity); //TODO make sure redirect is valid and doenst return error
+      if (post.status === 404) expect(post.status).to.equal(404); //could not create an activity to delete..
+
+      const get = await userSession.get(`/activity/b3b9f92b-3845-479b-a896-540c049ab1b3`);
+      if (get.status != 200) expect(get.status).to.not.equal(200);
+      console.log(get);
+    }); */
+  });
+});
--- api-server/src/routes/activity.ts
+++ api-server/src/routes/activity.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import { GenericDAO } from '../models/generic.dao.js';
 import { Activity } from '../models/activity.js';
 import { authService } from '../services/auth.service.js';
+import { MateMeet } from '../models/matemeet.js';
+import { UniversalDAO } from '../models/universal.dao.js';
+import { Meet } from '../models/meet.js';
 
 const router = express.Router();
 
@@ -9,10 +12,22 @@ const router = express.Router();
  * Returns all activities registered for a meetId
  */
 router.get('/:id', authService.authenticationMiddleware, async (req, res) => {
-  const activityDAO: GenericDAO<Activity> = req.app.locals.activityDAO;
-  const filter: Partial<Activity> = { meetid: req.params.id };
   const mateId = res.locals.user.id;
+  const activityDAO: GenericDAO<Activity> = req.app.locals.activityDAO;
+  const meetDAO: GenericDAO<Meet> = req.app.locals.meetDAO;
+  //check if meet is present
+  const meet: Meet | null = await meetDAO.findOne({ id: req.params.id });
+  if (meet === null || meet === undefined) res.status(404).end();
+  //end check
+  //check if user is in meet
+  const matemeetDAO: UniversalDAO<MateMeet> = req.app.locals.matemeetDAO;
+  const mateMeet = await matemeetDAO.findOne({ meetid: req.params.id, mateid: mateId });
+  if (mateMeet === null) res.status(403).end();
+  //end check
+
+  const filter: Partial<Activity> = { meetid: req.params.id };
   const activites = await activityDAO.findAll(filter);
+
   for (const e of activites) {
     e.image = Buffer.from(e.image as string).toString();
     if (mateId === e.tooltipcreatedby) e.deletepermission = true;
@@ -25,6 +40,17 @@ router.get('/:id', authService.authenticationMiddleware, async (req, res) => {
  * Adds a new activity to a meet
  */
 router.post('/:id', authService.authenticationMiddleware, async (req, res) => {
+  //check if meet is present
+  const meetDAO: GenericDAO<Meet> = req.app.locals.meetDAO;
+  const meet: Meet | null = await meetDAO.findOne({ id: req.params.id });
+  if (meet === null || meet === undefined) res.status(404).end(); //meet is not present
+  //end check
+  //check if user is in meet
+  const mateId = res.locals.user.id;
+  const matemeetDAO: UniversalDAO<MateMeet> = req.app.locals.matemeetDAO;
+  const mateMeet = await matemeetDAO.findOne({ meetid: req.params.id, mateid: mateId });
+  if (mateMeet === null) res.status(403).end(); //user is not in meet
+  //end check
   const activityDAO: GenericDAO<Activity> = req.app.locals.activityDAO;
   const utc = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
   const createdActivity = await activityDAO.create({
@@ -46,9 +72,29 @@ router.post('/:id', authService.authenticationMiddleware, async (req, res) => {
  * Deletes the activiyId with all corresponding ratings registered to this activity.
  */
 router.delete('/:id', authService.authenticationMiddleware, async (req, res) => {
+  //check if meet is present
+  const meetDAO: GenericDAO<Meet> = req.app.locals.meetDAO;
+  const meet: Meet | null = await meetDAO.findOne({ id: req.params.id });
+  if (meet === null || meet === undefined) res.status(404).end(); //meet is not present
+  //end check
+  //check if user is in meet
+  const mateId = res.locals.user.id;
+  const matemeetDAO: UniversalDAO<MateMeet> = req.app.locals.matemeetDAO;
+  const mateMeet = await matemeetDAO.findOne({ meetid: req.params.id, mateid: mateId });
+  if (mateMeet === null) res.status(403).end(); //user is not in meet
+  //end check
+
+  //TODO check if the user is allowed to delete this activity.
+
   const activityDAO: GenericDAO<Activity> = req.app.locals.activityDAO;
-  await activityDAO.delete(req.params.id); //table cascades to corresponding ratings, so no delete request necessary for ratings
-  res.status(200).end();
+  const filter: Partial<Activity> = { id: req.params.id };
+  const activity: Activity | null = await activityDAO.findOne(filter);
+  if (activity != null || activity != undefined) {
+    if (mateId === activity.tooltipcreatedby) await activityDAO.delete(req.params.id); //table cascades to corresponding ratings, so no delete request necessary for ratings
+    res.status(200).end();
+  } else {
+    res.status(404).end();
+  }
 });
 
 /**
